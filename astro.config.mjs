8d70d3cc2fe77fe556b268cdd3b7f714eac7118f
Maicol Rafal Rojas <rrojasmaicolnel@uss.edu.pe>
2025-09-09 18:33:03 -0500

    Corrijo importaciones TSX y configuro tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   base: '/Proyecto-Astro-Peru/', // <- ¡Cámbialo a exactamente ese nombre!
