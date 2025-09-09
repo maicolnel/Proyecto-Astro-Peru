26c042dc00f3c69bc0a01849e8d92dd7c2202857
Maicol Rafal Rojas <rrojasmaicolnel@uss.edu.pe>
2025-09-09 18:12:18 -0500

    Fix rutas agregando base para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  // Si vas a subir tu sitio a internet, reemplaza 'https://example.com' con tu dominio.
-  site: 'https://example.com',
-  integrations: [react(), sitemap()]
+  base: '/Proyecto-Astro-Peru/', // <- ¡Cámbialo a exactamente ese nombre!
 });
