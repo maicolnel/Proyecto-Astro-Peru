8c6b752e967ea63a6373951db764a578df369aa6
Maicol Rafal Rojas <rrojasmaicolnel@uss.edu.pe>
2025-09-10 21:13:36 -0500

    Configurar base para GitHub Pages 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://maicolnel.github.io/Proyecto-Astro-Peru',
   base: '/Proyecto-Astro-Peru/',
+  integrations: [react()],
 });
